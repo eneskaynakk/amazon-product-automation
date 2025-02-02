c81344246a5de7da527dbb30f7e5cf472e9c969c
EyupKaratay <159544340+EyupKaratay@users.noreply.github.com>
2025-02-02 23:22:44 +0300

    feature: updated category page v3

--- src/test/java/pages/CategoryPage.java
+++ src/test/java/pages/CategoryPage.java
@@ -26,6 +26,15 @@ public class CategoryPage {
     By dealsOfTheDayBrandFilter = By.cssSelector("span:nth-of-type(1) > div[data-a-input-name=\"brands\"]");
     By dealsOfTheDayProduct = By.xpath("(//span[@data-a-word-break=\"normal\"])[1]");
     By newReleasesProduct = By.xpath("(//li[@class=\"a-carousel-card\"])[19]");
+    By electronicBrandFilter = By.cssSelector("label[for=\"apb-browse-refinements-checkbox_4\"] > i");
+    By electronicStockFilter = By.cssSelector("div#p_n_availability a > span");
+    By electronicProduct = By.xpath("(//h2[@class=\"a-size-base-plus a-spacing-none a-color-base a-text-normal\"])[6]");
+    By giftIdeasProduct = By.xpath("(//h2[@class=\"a-size-base-plus a-spacing-none a-color-base a-text-normal\"])[14]");
+    By skinlessBookFilter = By.cssSelector("label[for=\"apb-browse-refinements-checkbox_16\"] > i");
+    By bookWriterFilter = By.xpath("//*[@id=\"p_lbr_books_authors_browse-bin/-Scholastic-\"]/span/a/div/label/i");
+    By bookLanguageFilter = By.xpath("//li[@id=\"p_n_feature_nine_browse-bin/14717274031\"]/span/a/div/label/i");
+    By bookStockFilter = By.cssSelector("div#p_n_availability i");
+    By bookProduct = By.xpath("(//div[@role=\"listitem\"])[4]/div/div/span/div/div/div/div[2]/div/div/div[1]");
 
     public void filterTheSearchedProduct(){
         elementControlPage.elementVisibilityV1(skin);
@@ -61,7 +70,28 @@ public class CategoryPage {
                 elementControlPage.elementVisibilityV1(newReleasesProduct);
                 productPage.addToCart();
             }
+            else if(i==5){
+                elementControlPage.elementVisibilityV1(subcategories);
+                elementControlPage.elementVisibilityV2(electronicBrandFilter);
+                elementControlPage.elementVisibilityV1(electronicStockFilter);
+                elementControlPage.elementVisibilityV1(electronicProduct);
+                productPage.addToCart();
+            }
+            else if(i==6){
+                elementControlPage.elementVisibilityV1(subcategories);
+                elementControlPage.elementVisibilityV1(giftIdeasProduct);
+                productPage.addToCart();
+            }
+            else{
+                elementControlPage.elementVisibilityV1(subcategories);
+                elementControlPage.elementVisibilityV1(skinlessBookFilter);
+                elementControlPage.elementVisibilityV1(bookWriterFilter);
+                elementControlPage.elementVisibilityV1(bookLanguageFilter);
+                elementControlPage.elementVisibilityV1(bookStockFilter);
+                elementControlPage.elementVisibilityV1(bookProduct);
+                productPage.addToCart();
+            }
+
         }
     }
-
 }
